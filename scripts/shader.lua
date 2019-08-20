7b61a3dd55d1f03614c754c150ca6d55479c6de2
ousttrue <ousttrue@gmail.com>
2019-08-20 12:48:15 +0900

    create_context

--- scripts/shader.lua
+++ scripts/shader.lua
@@ -57,9 +57,9 @@ float4 main(VS_OUTPUT _in): SV_Target
 define {
     {"VERTEX_POSITION", "aPosition", {}, {{"float3_t", "type"}}},
     {"vec4_w1", "vec4_w1", {{"float3_t", "type"}}, {{"float4_t", "type"}}},
-    {"SV_POSITION", "fPosition", {{"float4_t", "type"}}, {}},
+    {"VSOUT_POSITION", "fPosition", {{"float4_t", "type"}}, {}},
     {"const_float4_color", "const_color", {}, {{"value", "float4", "color"}}},
-    {"SV_TARGET", "result", {{"float4_t", "type"}}, {}}
+    {"PSOUT_COLOR", "result", {{"float4_t", "type"}}, {}}
 
     -- {"uniform_matrix", {}, {{"value", "mat4_t"}}}
     -- {"attr_position_vec3", {}, {{"vec3", "type"}}},
@@ -107,7 +107,7 @@ function createLink(src, dst)
 end
 local v_pos = createNode(node_definitions.VERTEX_POSITION, {10, 20})
 local to_v4 = createNode(node_definitions.vec4_w1, {210, 100})
-local sv_pos = createNode(node_definitions.SV_POSITION, {510, 180})
+local sv_pos = createNode(node_definitions.VSOUT_POSITION, {510, 180})
 createLink(v_pos, to_v4)
 createLink(to_v4, sv_pos)
 
@@ -121,22 +121,29 @@ function get_type(definition)
         return definition.type
     end
 end
+function get_semantic(name)
+    local pos = string.find(name, "_")
+    return string.sub(name, pos + 1)
+end
 function get_attribute(node)
     -- float3 aPosition: POSITION
     local type = get_type(node.definition.outputs[1])
     local name = node.name
-    local semantic = string.sub(node.definition.name, 8)
+    local semantic = get_semantic(node.definition.name)
     return string.format("%s %s : %s", type, name, semantic)
 end
 function get_vs_output(node)
     -- linear float4 fPosition: SV_POSITION
     local type = get_type(node.definition.inputs[1])
     local name = node.name
-    local semantic = node.definition.name
+    local semantic = get_semantic(node.definition.name)
+    if semantic == "POSITION" then
+        semantic = "SV_POSITION"
+    end
     return string.format("%s %s : %s", type, name, semantic)
 end
 function get_expression(lhs, rhs)
-    if lhs.definition.name == "SV_POSITION" then
+    if lhs.definition.name == "VSOUT_POSITION" then
         return string.format("return %s", rhs.name)
     end
     if lhs.definition.name == "vec4_w1" then
@@ -144,78 +151,84 @@ function get_expression(lhs, rhs)
     end
     return string.format("unknown expression: %s = %s", lhs, rhs)
 end
-function generate_vs(node)
-    print("-- generate_vs --")
-    local vs_context = {
-        attributes = {},
-        outputs = {},
+function create_context()
+    local self = {
         expressions = {},
         nodes = {}
     }
-    vs_context.add_node = function(node)
-        if node.definition.name == "VERTEX_POSITION" then
-            table.insert(vs_context.attributes, node)
-        elseif node.definition.name == "SV_POSITION" then
-            table.insert(vs_context.outputs, node)
-        end
+    self.add_node = function(node)
+        table.insert(self.nodes, node)
     end
-    vs_context.write_attributes = function(sb)
+    self.write_attributes = function(sb)
         table.insert(sb, "struct VS_INPUT\n")
         table.insert(sb, "{\n")
-        for i, x in ipairs(vs_context.attributes) do
-            table.insert(sb, string.format("    %s\n", get_attribute(x)))
+        for i, x in ipairs(self.nodes) do
+            if string.sub(x.definition.name, 1, 7) == "VERTEX_" then
+                table.insert(sb, string.format("    %s\n", get_attribute(x)))
+            end
         end
         table.insert(sb, "};\n")
         table.insert(sb, "\n")
     end
-    vs_context.write_out = function(sb)
+    self.write_vsout = function(sb)
         table.insert(sb, "struct VS_OUTPUT\n")
         table.insert(sb, "{\n")
-        for i, x in ipairs(vs_context.outputs) do
-            table.insert(sb, string.format("    %s\n", get_vs_output(x)))
+        for i, x in ipairs(self.nodes) do
+            if string.sub(x.definition.name, 1, 6) == "VSOUT_" then
+                table.insert(sb, string.format("    %s\n", get_vs_output(x)))
+            end
         end
         table.insert(sb, "};\n")
         table.insert(sb, "\n")
     end
-    vs_context.add_expression = function(lhs, rhs)
-        table.insert(vs_context.expressions, {lhs, rhs})
+    self.add_expression = function(lhs, rhs)
+        table.insert(self.expressions, {lhs, rhs})
     end
-    vs_context.write_expressions = function(sb)
+    self.write_expressions = function(sb)
         -- main
-        table.insert(sb, "VS_OUTPUT main(VS_INPUT _in)\n")
-        table.insert(sb, "{\n")
-        table.insert(sb, "    VS_OUTPUT ret;\n")
-        for i, x in ipairs(vs_context.expressions) do
+        for i, x in ipairs(self.expressions) do
             local lhs = x[1]
             local rhs = x[2]
             table.insert(sb, string.format("    %s;\n", get_expression(lhs, rhs)))
         end
-        table.insert(sb, "}\n")
-        table.insert(sb, "")
     end
 
+    self.to_vs = function(sb)
+        local sb = {}
+        self.write_attributes(sb)
+        self.write_vsout(sb)
+        -- main
+        table.insert(sb, "VS_OUTPUT main(VS_INPUT _in)\n")
+        table.insert(sb, "{\n")
+        table.insert(sb, "    VS_OUTPUT ret;\n")
+        self.write_expressions(sb)
+        table.insert(sb, "}\n")
+        table.insert(sb, "")
+        return table.concat(sb, "")
+    end
+
+    return self
+end
+function generate_vs(node)
+    print("-- generate_vs --")
+    local context = create_context()
     -- build tree
     function traverse(node)
-        vs_context.add_node(node)
+        context.add_node(node)
         for i, slot in ipairs(node.inslots) do
             local src = slot.get_src_node()
             if src then
                 traverse(src)
-                vs_context.add_expression(node, src)
+                context.add_expression(node, src)
             end
         end
     end
     traverse(node)
-
-    local sb = {}
-    vs_context.write_attributes(sb)
-    vs_context.write_out(sb)
-    vs_context.write_expressions(sb)
-
-    return table.concat(sb, "")
+    -- export
+    return context.to_vs()
 end
 function eval_node(node)
-    if node.definition.name == "SV_POSITION" then
+    if node.definition.name == "VSOUT_POSITION" then
         local vs = generate_vs(node)
         print(vs)
     elseif node.name == "SV_TARGET" then
